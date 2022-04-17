94d5abb4469f4ede915711a80f82d9dacaf96af7
Rishith-maker-1 <83953765+Rishith-maker-1@users.noreply.github.com>
2022-04-17 18:36:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function a(){
+    window.location="https://rishith-maker-1.github.io/lms";
+}
+function b(){
+    window.location="https://rishith-maker-1.github.io/lb2";
+}
+function c(){
+    window.location="https://rishith-maker-1.github.io/pc";
+}
+function d(){
+    window.location="https://rishith-maker-1.github.io/hp1-4";
+}
+function e(){
+    window.location="https://rishith-maker-1.github.io/hp5-7";
+}
+function f(){
+    window.location="https://rishith-maker-1.github.io/lij-toa";
+}
+function g(){
+    window.location="https://rishith-maker-1.github.io/lij-tac";
+}
+function h(){
+    window.location="https://rishith-maker-1.github.io/llotr";
+}
+function i(){
+    window.location="https://rishith-maker-1.github.io/lsw3";
+}
+function j(){
+    window.location="https://rishith-maker-1.github.io/lsw-tcs";
+}
+function k(){
+    window.location="https://rishith-maker-1.github.io/lb1";
+}
