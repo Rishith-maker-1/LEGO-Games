9c1e920dba43b9519cfb6bd6cac73d4351f9c060
Rishith-maker-1 <83953765+Rishith-maker-1@users.noreply.github.com>
2023-03-07 19:24:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,77 +3,77 @@ function a(){
     document.getElementById('IFRAME').src = link;
     document.getElementById('s2').style.display='block'; //to show
     document.getElementById('s1').style.display='none'; //to hide
-    document.getElementById('Name').innerHTML='LEGO';
+    document.getElementById('Name').innerHTML='LEGO Marvel Superheroes';
 }
 function b(){
     link="https://rishith-maker-1.github.io/lb2";
     document.getElementById('IFRAME').src = link;
     document.getElementById('s2').style.display='block'; //to show
     document.getElementById('s1').style.display='none'; //to hide
-    document.getElementById('Name').innerHTML='LEGO';
+    document.getElementById('Name').innerHTML='LEGO Batman 2';
 }
 function c(){
     link="https://rishith-maker-1.github.io/pc";
     document.getElementById('IFRAME').src = link;
     document.getElementById('s2').style.display='block'; //to show
     document.getElementById('s1').style.display='none'; //to hide
-    document.getElementById('Name').innerHTML='LEGO';
+    document.getElementById('Name').innerHTML='LEGO Pirates of the Caribbean';
 }
 function d(){
     link="https://rishith-maker-1.github.io/hp1-4";
     document.getElementById('IFRAME').src = link;
     document.getElementById('s2').style.display='block'; //to show
     document.getElementById('s1').style.display='none'; //to hide
-    document.getElementById('Name').innerHTML='LEGO';
+    document.getElementById('Name').innerHTML='LEGO Harry Potter Years 1-4';
 }
 function e(){
     link="https://rishith-maker-1.github.io/hp5-7";
     document.getElementById('IFRAME').src = link;
     document.getElementById('s2').style.display='block'; //to show
     document.getElementById('s1').style.display='none'; //to hide
-    document.getElementById('Name').innerHTML='LEGO';
+    document.getElementById('Name').innerHTML='LEGO Harry Potter Years 5-7';
 }
 function f(){
     link="https://rishith-maker-1.github.io/lij-toa";
     document.getElementById('IFRAME').src = link;
     document.getElementById('s2').style.display='block'; //to show
     document.getElementById('s1').style.display='none'; //to hide
-    document.getElementById('Name').innerHTML='LEGO';
+    document.getElementById('Name').innerHTML='LEGO Indiana Jones-The Original Adventures';
 }
 function g(){
     link="https://rishith-maker-1.github.io/lij2-tac";
     document.getElementById('IFRAME').src = link;
     document.getElementById('s2').style.display='block'; //to show
     document.getElementById('s1').style.display='none'; //to hide
-    document.getElementById('Name').innerHTML='LEGO';
+    document.getElementById('Name').innerHTML='LEGO Indiana Jones 2-The Adventure Continues';
 }
 function h(){
     link="https://rishith-maker-1.github.io/llotr";
     document.getElementById('IFRAME').src = link;
     document.getElementById('s2').style.display='block'; //to show
     document.getElementById('s1').style.display='none'; //to hide
-    document.getElementById('Name').innerHTML='LEGO';
+    document.getElementById('Name').innerHTML='LEGO Lord Of The Rings';
 }
 function i(){
     link="https://rishith-maker-1.github.io/lsw3";
     document.getElementById('IFRAME').src = link;
     document.getElementById('s2').style.display='block'; //to show
     document.getElementById('s1').style.display='none'; //to hide
-    document.getElementById('Name').innerHTML='LEGO';
+    document.getElementById('Name').innerHTML='LEGO Star Wars 3-The Clone Wars';
 }
 function j(){
     link="https://rishith-maker-1.github.io/lsw-tcs";
     document.getElementById('IFRAME').src = link;
     document.getElementById('s2').style.display='block'; //to show
     document.getElementById('s1').style.display='none'; //to hide
-    document.getElementById('Name').innerHTML='LEGO';
+    document.getElementById('Name').innerHTML='LEGO Star Wars-The Complete Saga';
 }
 function k(){
     link="https://rishith-maker-1.github.io/lb1";
     document.getElementById('IFRAME').src = link;
     document.getElementById('s2').style.display='block'; //to show
     document.getElementById('s1').style.display='none'; //to hide
-    document.getElementById('Name').innerHTML='LEGO';
+    document.getElementById('Name').innerHTML='LEGO Batman-The Videogame';
 }
 function goback(){
     document.getElementById('s1').style.display='block'; //to show
