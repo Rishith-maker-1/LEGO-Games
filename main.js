0a4383a3b71bd4374e70edf65bc1dbfdf0a85086
Rishith-maker-1 <83953765+Rishith-maker-1@users.noreply.github.com>
2023-03-07 19:10:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,77 +3,77 @@ function a(){
     document.getElementById('IFRAME').src = link;
     document.getElementById('s2').style.display='block'; //to show
     document.getElementById('s1').style.display='none'; //to hide
-    document.getElementById('name').innerHTML="LEGO";
+    document.getElementById('name').innerHTML='LEGO';
 }
 function b(){
     link="https://rishith-maker-1.github.io/lb2";
     document.getElementById('IFRAME').src = link;
     document.getElementById('s2').style.display='block'; //to show
     document.getElementById('s1').style.display='none'; //to hide
-    document.getElementById('name').innerHTML="LEGO";
+    document.getElementById('name').innerHTML='LEGO';
 }
 function c(){
     link="https://rishith-maker-1.github.io/pc";
     document.getElementById('IFRAME').src = link;
     document.getElementById('s2').style.display='block'; //to show
     document.getElementById('s1').style.display='none'; //to hide
-    document.getElementById('name').innerHTML="LEGO";
+    document.getElementById('name').innerHTML='LEGO';
 }
 function d(){
     link="https://rishith-maker-1.github.io/hp1-4";
     document.getElementById('IFRAME').src = link;
     document.getElementById('s2').style.display='block'; //to show
     document.getElementById('s1').style.display='none'; //to hide
-    document.getElementById('name').innerHTML="LEGO";
+    document.getElementById('name').innerHTML='LEGO';
 }
 function e(){
     link="https://rishith-maker-1.github.io/hp5-7";
     document.getElementById('IFRAME').src = link;
     document.getElementById('s2').style.display='block'; //to show
     document.getElementById('s1').style.display='none'; //to hide
-    document.getElementById('name').innerHTML="LEGO";
+    document.getElementById('name').innerHTML='LEGO';
 }
 function f(){
     link="https://rishith-maker-1.github.io/lij-toa";
     document.getElementById('IFRAME').src = link;
     document.getElementById('s2').style.display='block'; //to show
     document.getElementById('s1').style.display='none'; //to hide
-    document.getElementById('name').innerHTML="LEGO";
+    document.getElementById('name').innerHTML='LEGO';
 }
 function g(){
     link="https://rishith-maker-1.github.io/lij2-tac";
     document.getElementById('IFRAME').src = link;
     document.getElementById('s2').style.display='block'; //to show
     document.getElementById('s1').style.display='none'; //to hide
-    document.getElementById('name').innerHTML="LEGO";
+    document.getElementById('name').innerHTML='LEGO';
 }
 function h(){
     link="https://rishith-maker-1.github.io/llotr";
     document.getElementById('IFRAME').src = link;
     document.getElementById('s2').style.display='block'; //to show
     document.getElementById('s1').style.display='none'; //to hide
-    document.getElementById('name').innerHTML="LEGO";
+    document.getElementById('name').innerHTML='LEGO';
 }
 function i(){
     link="https://rishith-maker-1.github.io/lsw3";
     document.getElementById('IFRAME').src = link;
     document.getElementById('s2').style.display='block'; //to show
     document.getElementById('s1').style.display='none'; //to hide
-    document.getElementById('name').innerHTML="LEGO";
+    document.getElementById('name').innerHTML='LEGO';
 }
 function j(){
     link="https://rishith-maker-1.github.io/lsw-tcs";
     document.getElementById('IFRAME').src = link;
     document.getElementById('s2').style.display='block'; //to show
     document.getElementById('s1').style.display='none'; //to hide
-    document.getElementById('name').innerHTML="LEGO";
+    document.getElementById('name').innerHTML='LEGO';
 }
 function k(){
     link="https://rishith-maker-1.github.io/lb1";
     document.getElementById('IFRAME').src = link;
     document.getElementById('s2').style.display='block'; //to show
     document.getElementById('s1').style.display='none'; //to hide
-    document.getElementById('name').innerHTML="LEGO";
+    document.getElementById('name').innerHTML='LEGO';
 }
 function goback(){
     document.getElementById('s1').style.display='block'; //to show
